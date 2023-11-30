71a1364fa4e59f74e2bdb0f861efe2d83303923e
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-30 15:06:07 +0100

    milestone-2-complete

--- functions/functions.php
+++ functions/functions.php
@@ -1,4 +1,25 @@
 <?php
 
+function passwordGenerator()
+{
+    $symbols = '!?&%$<>^+-\*/()[]{}@#_=';
+    $letters = 'qwertyuiopasdfghjklzxcvbnm';
+    $upLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+    $numbers = '0123456789';
+
+    if (isset($_GET['passwordLength'])) {
+        $passwordLength = $_GET['passwordLength'];
+        $newPassword = '';
+        while (strlen($newPassword) < $passwordLength) {
+            $charactersList = $symbols . $letters . $upLetters . $numbers;
+            $newCharacter = $charactersList[rand(0, strlen($charactersList) - 1)];
+            if (!str_contains($newPassword, $newCharacter)) {
+                $newPassword .= $newCharacter;
+            }
+        }
+        var_dump($_GET['passwordLength']);
+        var_dump($newPassword);
+    }
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 include __DIR__ . '/partials/header.php';
 
-
-$symbols = '!?&%$<>^+-\*/()[]{}@#_=';
-$letters = 'qwertyuiopasdfghjklzxcvbnm';
-$upLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM';
-$numbers = '0123456789';
-
-if (isset($_GET['passwordLength'])) {
-    $passwordLength = $_GET['passwordLength'];
-    $newPassword = '';
-    while (strlen($newPassword) < $passwordLength) {
-        $charactersList = $symbols . $letters . $upLetters . $numbers;
-        $newCharacter = $charactersList[rand(0, strlen($charactersList) - 1)];
-        if (!str_contains($newPassword, $newCharacter)) {
-            $newPassword .= $newCharacter;
-        }
-    }
-    var_dump($_GET['passwordLength']);
-    var_dump($newPassword);
-}
-
+passwordGenerator();
 ?>
 
 <main class="container">
